5c6c297d836294b2764284396df4a176bb947165
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2025-12-18 13:58:18 +0000

    Create a skeleton custom.js

---
+++ dashboard/static/dashboard/custom.js
@@ -0,0 +1,10 @@
+/*
+Dashboard JavaScript.
+
+Week 1 intent:
+- keep the UI simple and predictable
+- avoid client-side state that can diverge from server-side scoring
+*/
+(function () {
+  "use strict";
+})();
